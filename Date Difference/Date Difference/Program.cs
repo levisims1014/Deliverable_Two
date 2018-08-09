29f31c1e46daa76450f7f9f42daabce4570d7d93
levisims1014 <42224339+levisims1014@users.noreply.github.com>
2018-08-09 09:46:13 -0400

    First commit: Lab 1: Part 2 Dates

---
+++ Date Difference/Date Difference/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //declare variables
+            DateTime LeviDate1;
+            DateTime LeviDate2;
+
+
+            //asking for user input
+            Console.WriteLine("enter a date mm/dd/yyyy: ");
+            //getting user input and coverting info to date
+            LeviDate1 = Convert.ToDateTime(Console.ReadLine());
+
+
+            //asking for user input
+            Console.WriteLine("Enter a date mm/dd/yyyy: ");
+            //getting user input and coverting info to date
+            LeviDate2 = Convert.ToDateTime(Console.ReadLine());
+
+            //getting the difference of the dates and naming the variable for it
+            var diff = LeviDate2 - LeviDate1;
+            //printing the difference of the dates in days
+            Console.WriteLine("diff.Days: " + diff.Days);
+            //printing the differences of the dates in hours
+            Console.WriteLine("diff.Hours: " + diff.Hours);
+            //printing the difference of the dates in minutes
+            Console.WriteLine("diff.Minutes: " + diff.Minutes);
+
+            Console.ReadLine();
+        }
+    }
+}
